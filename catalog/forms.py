6d7fb6ad5d19a91b20efff6cfa00f64f31f596b4
Maksim Lakovich <maks.lakovich@gmail.com>
2025-02-11 16:24:41 +0300

    В форме ProductForm (catalog/forms.py) создан метод clean_image() для валидации формата (JPEG/PNG) и размера (до 5 МБ) загружаемого пользователем изображения.

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,7 @@ import re
 from django import forms
 from django.core.exceptions import ValidationError
 
-from config.config import FORBIDDEN_WORDS
+from config.config import ALLOWED_IMAGE_FORMATS, FORBIDDEN_WORDS, MAX_IMAGE_SIZE
 from .models import Product
 
 
@@ -68,6 +68,16 @@ class ProductForm(forms.ModelForm):
             field_label = self.fields['price'].label
             raise ValidationError(f'Поле "{field_label}" не может быть отрицательным.')
         return price
+
+    def clean_image(self):
+        """Валидацию формата (JPEG/PNG) и размера (до 5 МБ) загружаемого пользователем изображения."""
+        image = self.cleaned_data.get('image')
+        if image:
+            if image.content_type not in ALLOWED_IMAGE_FORMATS:
+                raise ValidationError('Файл должен быть в формате JPEG или PNG.')
+            if image.size > MAX_IMAGE_SIZE:
+                raise ValidationError('Размер изображения не должен превышать 5 МБ.')
+        return image
 
 
 class ContactForm(forms.Form):
