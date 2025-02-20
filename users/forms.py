05792bbdf51523ccac3251d87e552e22bf7bdb2a
Maksim Lakovich <maks.lakovich@gmail.com>
2025-02-20 03:14:41 +0300

    В "сatalog/forms.py" создана форма UserCustomerRegistrationForm(UserCreationForm) для регистрации пользователя на сайте магазина на странице register.html

---
+++ users/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from users.models import UserCustomer
+
+
+class UserCustomerRegistrationForm(UserCreationForm):
+    """Форма для регистрации пользователя на сайте магазина."""
+
+    class Meta:
+        model = UserCustomer
+        fields = ("email", "password1", "password2")
+        widgets = {
+            "email": forms.EmailInput(attrs={"placeholder": "Введите email"}),
+            "password1": forms.PasswordInput(attrs={"placeholder": "Введите пароль"}),
+            "password2": forms.PasswordInput(attrs={"placeholder": "Введите пароль повторно"}),
+        }
+
+    def clean_email(self):
+        """Переопределение clean_email() для явной проверки уникальности email.
+        Функция clean_email() нужна, даже если email уже unique=True в модели как у нас потому что unique=True в
+        модели выбрасывает ошибку на уровне БД, но не показывает ее в форме, а clean_email() помогает показать
+        пользователю в форме, что такой email уже занят."""
+        email = self.cleaned_data.get("email")
+        if UserCustomer.objects.filter(email=email).exists():
+            raise ValidationError("Этот email уже используется. Пожалуйста, выберите другой.")
+        return email
+
+    def __init__(self, *args, **kwargs):
+        """Добавляю CSS-классы ко всем полям формы. Убираем 'help_text' для всех полей, чтоб это больше не выводилось
+        по умолчанию на html-странице."""
+        super().__init__(*args, **kwargs)
+
+        # ШАГ 1: Убираю help_text из вывода на странице, так как help_text из model.py и дублирует то,
+        # что и так уже автоматически создает class UserCustomer(AbstractUser).
+        for field_name, field in self.fields.items():
+            field.help_text = None
+            # ШАГ 2: Добавляю класс "form-control" для всех полей.
+            if not isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] = "form-control"
+            # ШАГ 2: Добавляю placeholder вручную для наших полей формы регистрации.
+            placeholders = {
+                "email": "Введите email",
+                "password1": "Введите пароль",
+                "password2": "Введите пароль повторно",
+            }
+            if field_name in placeholders:
+                field.widget.attrs["placeholder"] = placeholders[field_name]
--- users/models.py
+++ users/models.py
@@ -1,17 +1,39 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from phonenumber_field.modelfields import PhoneNumberField
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField  # type: ignore
 
 
 class UserCustomer(AbstractUser):
     """Модель UserCustomer представляет пользователя магазина."""
-    email = models.EmailField(unique=True, verbose_name="Email", help_text="Введите email")
-    avatar = models.ImageField(upload_to="user_avatar", blank=True, null=True, verbose_name="Аватар пользователя", help_text="Загрузите аватар")
-    phone_number = PhoneNumberField(blank=True, null=True, verbose_name="Телефон пользователя", help_text="Введите телефон")
-    country = models.CharField(max_length=50, blank=True, null=True, verbose_name="Страна пользователя", help_text="Укажите страну")
+
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Введите email"
+    )
+    avatar = models.ImageField(
+        upload_to="user_avatar",
+        blank=True,
+        null=True,
+        verbose_name="Аватар пользователя",
+        help_text="Загрузите аватар",
+    )
+    phone_number = PhoneNumberField(
+        blank=True,
+        null=True,
+        verbose_name="Телефон пользователя",
+        help_text="Введите телефон",
+    )
+    country = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Страна пользователя",
+        help_text="Укажите страну",
+    )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username",]
+    REQUIRED_FIELDS = [
+        "username",
+    ]
 
     def __str__(self):
         """Метод определяет строковое представление объекта. Полезно для отображения объектов в админке/консоли."""
