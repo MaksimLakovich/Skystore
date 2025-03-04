8622558afbabb9d7301cc9e7b0cf2e983b8ffaf0
Maksim Lakovich <maks.lakovich@gmail.com>
2025-03-04 16:26:21 +0300

    В create_groups.py доработана кастомная команда для создания группы 'Модератор продуктов' (добавлено право на удаление продукта - delete_product)

--- users/management/commands/create_groups.py
+++ users/management/commands/create_groups.py
@@ -6,10 +6,13 @@ class Command(BaseCommand):
     help = "Создает группу 'Модератор продуктов' и назначает ей права."
 
     def handle(self, *args, **kwargs):
-        # Создаю или получаю, если уже есть, группу "Модератор продуктов"
+        # Создаю или получаю, если уже есть, группу "Модератор продуктов":
         group, created = Group.objects.get_or_create(name="Модератор продуктов")
-        can_change_publication = Permission.objects.get(codename="can_change_product_publication")  # Получаю права
-        group.permissions.add(can_change_publication)  # Добавляю права в группу
+        # Получаю предустановленное право для удаления продукта и кастомное право на управление публикацией продуктов:
+        can_change_publication = Permission.objects.get(codename="can_change_product_publication")
+        can_delete_product = Permission.objects.get(codename="delete_product")
+        # Добавляю права в нашу группу:
+        group.permissions.add(can_change_publication, can_delete_product)
 
         if created:
             self.stdout.write(self.style.SUCCESS("Группа 'Модератор продуктов' создана и права добавлены!"))
